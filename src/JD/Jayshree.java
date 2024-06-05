5f18707a41e8ab29bf2776d7c645f5186536bc35
jayshree-dolase <jayshreedolase3@gmail.com>
2024-06-05 15:25:18 +0530

    first commit

---
+++ src/JD/Jayshree.java
@@ -0,0 +1,16 @@
+package JD;
+
+public class Jayshree {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+		System.out.println("Rutu");
+		for(int i=0;i<=10;i++){
+			int no=5;
+			System.out.println("no*"+i+"="+no*i);
+	}
+	
+System.out.println("Radhe Radhe");
+}
+}
